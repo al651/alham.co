d4ddfea9f67f4d3dffdad9d34765794412408282
Ali Hamid <75144736+al651@users.noreply.github.com>
2023-03-14 04:27:55 +0000

    removing unused css + refactors + post prep

--- src/components/BlogPostButton.js
+++ src/components/BlogPostButton.js
@@ -5,15 +5,15 @@ function BlogPost(props){
     const title = props.title;
     const description = props.description;
     const img = props.img;
-    const href = ('blog' + props.href);
+    const slug = ('blog' + props.slug);
     const alt = props.imgalt;
     const date = props.date;
     
     return (
         <div className='blog-post-wrapper'>
             <div className="blog-card">
-            <a href={href}><img src={img} alt= {alt} className='postImg'></img></a>
-            <h3><a class='blog-title' href={href}>{title}</a></h3>
+            <a href={slug}><img src={img} alt= {alt} className='postImg'></img></a>
+            <h3><a class='blog-title' href={slug}>{title}</a></h3>
             <div className='blog-post-content-wrapper'>
             <p className='description-text'>
                 {description}
